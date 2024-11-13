8c7a0651d09dc6492f916ae8c717eea2d46050fa
Gen Xu <gen@nimble.ai>
2024-11-12 19:16:05 -0800

    expose max_frame_size for h2

--- tonic/src/transport/channel/endpoint.rs
+++ tonic/src/transport/channel/endpoint.rs
@@ -44,6 +44,7 @@ pub struct Endpoint {
     pub(crate) http2_keep_alive_timeout: Option<Duration>,
     pub(crate) http2_keep_alive_while_idle: Option<bool>,
     pub(crate) http2_max_header_list_size: Option<u32>,
+    pub(crate) http2_max_frame_size: Option<u32>,
     pub(crate) connect_timeout: Option<Duration>,
     pub(crate) http2_adaptive_window: Option<bool>,
     pub(crate) local_address: Option<IpAddr>,
@@ -89,6 +90,7 @@ impl Endpoint {
             http2_keep_alive_timeout: None,
             http2_keep_alive_while_idle: None,
             http2_max_header_list_size: None,
+            http2_max_frame_size: None,
             connect_timeout: None,
             http2_adaptive_window: None,
             executor: SharedExec::tokio(),
@@ -116,6 +118,7 @@ impl Endpoint {
             http2_keep_alive_timeout: None,
             http2_keep_alive_while_idle: None,
             http2_max_header_list_size: None,
+            http2_max_frame_size: None,
             connect_timeout: None,
             http2_adaptive_window: None,
             executor: SharedExec::tokio(),
@@ -390,6 +393,14 @@ impl Endpoint {
     pub fn http2_max_header_list_size(self, size: u32) -> Self {
         Endpoint {
             http2_max_header_list_size: Some(size),
+            ..self
+        }
+    }
+
+    /// Sets the max size of received frames.
+    pub fn http2_max_frame_size(self, size: u32) -> Self {
+        Endpoint {
+            http2_max_frame_size: Some(size),
             ..self
         }
     }
